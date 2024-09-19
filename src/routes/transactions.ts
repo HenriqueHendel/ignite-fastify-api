edbad9cb202887f253c92a8c89903512a0d70ebc
Henrique Lopes <henriquehendell@gmail.com>
2024-09-19 00:09:08 -0300

    feat: route to get transaction by id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -9,6 +9,21 @@ export async function transactionRoutes(app: FastifyInstance) {
     return { transactions }
   })
 
+  app.get('/:id', async (request) => {
+    const getTransactionByIdSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = getTransactionByIdSchema.parse(request.params)
+
+    const transaction = await knex('transactions')
+      .where({
+        id,
+      })
+      .first()
+
+    return { transaction }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
